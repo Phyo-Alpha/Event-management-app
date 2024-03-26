5b9a6c72c97f7cf77ead669417a2d36a2f3f95f9
Phyo-Alpha <77912443+Phyo-Alpha@users.noreply.github.com>
2024-03-27 02:44:09 +0800

    Minor Bug fixes

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -2,21 +2,19 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || {conn: null, promise: null};
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
- 
-    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if (cached.conn) return cached.conn;
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName : 'evently',
-        bufferCommands: false,
-    })
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    cached.conn = await cached.promise;
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-    return cached.conn;
-};
+  cached.conn = await cached.promise;
 
-// Server actions 
+  return cached.conn;
+}
